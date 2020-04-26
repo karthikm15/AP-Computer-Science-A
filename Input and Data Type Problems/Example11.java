3772154b395279631f1e532c64716b9cd8eb0791
karthikm15 <50070778+karthikm15@users.noreply.github.com>
2020-04-26 08:17:28 -0700

    Create Example11.java

---
+++ Input and Data Type Problems/Example11.java
@@ -0,0 +1,8 @@
+import java.util.Random;
+public class Chap2E11 {
+	public static void main(String[] args) {
+    	Random number = new Random();
+    	int n = number.nextInt(5);
+    	System.out.println(n);
+	}
+}
