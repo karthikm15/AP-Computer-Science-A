65c05287a291cc2c989cf5d9869ed508054c9c45
karthikm15 <50070778+karthikm15@users.noreply.github.com>
2020-05-24 08:14:00 -0700

    Problem 6

---
+++ AP Problems/Problem8.java
@@ -0,0 +1,23 @@
+1) a)
+
+public int numWordsofLength (int len) {
+    int count = 0;
+    for (int i = 0; i < myList.size(); i++) {
+        if (myList.get(i).length == len) {
+            count++;
+        }
+    }
+    return count;
+}
+
+b)
+public void removeWordsOfLength (int len) {
+    ArrayList<String> arrList = new ArrayList<String>();
+    for (int i = 0; i < myList.size(); i++) {
+        if (myList.get(i).length != len) {
+            arrList.add(myList.get(i));
+        }
+    }
+    myList = arrList;
+}
+
