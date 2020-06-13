c683b39c8d48c39807234b886021e02d1a1db593
karthikm15 <50070778+karthikm15@users.noreply.github.com>
2020-06-13 08:09:46 -0700

    AP Exam Problem 3

---
+++ AP Problems/APExamProblem3.java
@@ -0,0 +1,29 @@
+import java.util.*;
+// APCSA Question 2, part a 
+public static boolean runSimulation (int sampleSize) {
+    int firsthalf = 0;
+    for (int i = 0; i < sampleSize/2; i++) {
+        int num = getInt();
+        // I am considering 0 to be a positive value.
+        if ((isTarget(num)) && (num >= 0)) {
+            firsthalf++;
+        }
+    }
+    int secondhalf = 0;
+    for (int i = sampleSize/2; i < sampleSize; i++) {
+        int number = getInt();
+        if ((isTarget(number))  && (number >= 0)) {
+            secondhalf++;
+        }
+    }
+    return firsthalf > secondhalf;
+}
+
+// APCSA Question 2, part b 
+First, I would put a private instance variable in the beginning of the class called numValues.
+Then, I would create a new method: public TargetIntegers (int input). The input 
+would be provided by the user when he is declaring the class. 
+I would then set numValues to be equal to input 
+inside of this method. Additionally, now, there would be no use for a SampleSize parameter
+inside of the runSimulation() method because there is already a variable 
+that tells the sample size.
